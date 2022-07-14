eec27d88f90473d890b1c4e6029a48a0756fe9ba
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-07-14 14:12:58 +0300

    Updated

---
+++ T02.VehiclesExtension/Models/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T02.VehiclesExtension
+{
+    public class Truck : Vehicle
+    {
+        private const double TruckFuelConsumptionIncrement = 1.6;
+        private const double RefuelCoeffiecient = 0.95;
+
+        //TODO: Fix truck Refuel (950 -> 1000) and Fuel Quantity.
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+
+        }
+
+        protected override double FuelConsumptionModifier
+            => TruckFuelConsumptionIncrement;
+
+        public override void Refuel(double liters)
+        {
+            base.Refuel(liters * RefuelCoeffiecient);
+        }
+    }
+}
